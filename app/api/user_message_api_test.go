94eda34cb1de713fb22a3e014b2ea6188597a938
tomjaroszewskiwork <tomjaroszewskiwork@gmail.com>
2018-08-31 15:58:54 -0400

    Added test cases for POST

--- app/api/user_message_api_test.go
+++ app/api/user_message_api_test.go
@@ -1,9 +1,11 @@
 package api
 
 import (
-	"fmt"
+	"encoding/json"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	store "github.com/tomjaroszewskiwork/go-user-messages/app/store"
 	test "github.com/tomjaroszewskiwork/go-user-messages/test"
@@ -18,11 +20,27 @@ func TestMain(m *testing.M) {
 }
 
 func TestGetMessage(t *testing.T) {
-	wd, _ := os.Getwd()
-	fmt.Println(wd)
-	test.CodeTest(t, "GET", "/v1/users/fake.tom.j/messages/100", 404)
-	test.CodeTest(t, "GET", "/v1/users/tom.j1/messages/11000", 404)
-	test.CodeTest(t, "GET", "/v1/users/tom.j1/messages/-1", 404)
-	test.CodeTest(t, "GET", "/v1/users/tom.j1/messages/abc", 400)
+	test.CodeTest(t, "GET", "/v1/users/fake.tom.j/messages/100", nil, 404)
+	test.CodeTest(t, "GET", "/v1/users/tom.j1/messages/11000", nil, 404)
+	test.CodeTest(t, "GET", "/v1/users/tom.j1/messages/-1", nil, 404)
+	test.CodeTest(t, "GET", "/v1/users/tom.j1/messages/abc", nil, 400)
 	test.BodyResponseTest(t, "GET", "/v1/users/tom.j/messages/100", nil, 200, "message")
+}
+
+func TestAddMessage(t *testing.T) {
+	test.CodeTest(t, "POST", "/v1/users/tom.j/messages", nil, 400)
+	messageBody := MessageBody{Message: ""}
+	test.CodeTest(t, "POST", "/v1/users/tom.j/messages", messageBody, 400)
+	messageBody.Message = "values"
+	response := test.CodeTest(t, "POST", "/v1/users/tom.j/messages", messageBody, 201)
+	newLocation := response.HeaderMap.Get("Location")
+	response = test.CodeTest(t, "GET", newLocation, nil, 200)
+	// Can't do auto JSON compare as time will differ, freezing time is too complicated
+	var newMessage store.UserMessage
+	err := json.NewDecoder(response.Body).Decode(&newMessage)
+	if err != nil {
+		require.FailNow(t, err.Error())
+	}
+	require.Equal(t, "tom.j", newMessage.UserID)
+	require.Equal(t, messageBody.Message, newMessage.Message)
 }
--- test/test_api.go
+++ test/test_api.go
@@ -25,12 +25,12 @@ func SetTestRouter(newRouter *mux.Router) {
 }
 
 // CodeTest makes sure that the return code matches
-func CodeTest(t *testing.T, method string, url string, expectedCode int) {
-	responseTest(t, method, url, nil, expectedCode)
+func CodeTest(t *testing.T, method string, url string, body interface{}, expectedCode int) *httptest.ResponseRecorder {
+	return responseTest(t, method, url, body, expectedCode)
 }
 
 // BodyResponseTest calls the API and compares the full body return
-func BodyResponseTest(t *testing.T, method string, url string, body interface{}, exectedCode int, expectedResponseFile string) {
+func BodyResponseTest(t *testing.T, method string, url string, body interface{}, exectedCode int, expectedResponseFile string) *httptest.ResponseRecorder {
 	response := responseTest(t, method, url, body, exectedCode)
 	// Does a JSON diff on the body compared to the expected JSON file
 	differ := diff.New()
@@ -48,6 +48,7 @@ func BodyResponseTest(t *testing.T, method string, url string, body interface{},
 		diffString, _ := formatter.Format(delta)
 		require.FailNow(t, diffString)
 	}
+	return response
 }
 
 // Calls the API, tests for the code and returns the response
@@ -56,7 +57,9 @@ func responseTest(t *testing.T, method string, url string, body interface{}, exp
 	var err error
 	if body != nil {
 		bodyJSON, err = json.Marshal(body)
-		require.NotNil(t, err)
+		if err != nil {
+			require.FailNow(t, err.Error())
+		}
 	}
 	req, _ := http.NewRequest(method, url, bytes.NewBuffer(bodyJSON))
 	response := httptest.NewRecorder()
